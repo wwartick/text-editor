a8c7f10a643e129712ba13f36d2cfc96fb46bd2b
wwartick <willwartick@gmail.com>
2022-11-30 21:23:50 -0800

    database methods

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  
+  const newDb = await openDB('jate', 1);
+  const tx = newDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id:1, content: content});
+  const result = await request;
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+
+  const newDb = await openDB('jate', 1);
+  const tx = newDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id:1, content: content});
+  const result = await request;
+}
+
+}
+
 
 initdb();
